f45fcc73fdd9d1a48721c9b2010599b39a96b97a
Lexi <83599097+lexim515@users.noreply.github.com>
2022-02-02 10:15:45 -0500

    Update Posts.js

--- Posts.js
+++ Posts.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const BASE_URL =
-  "https://strangers-things.herokuapp.com/api/2110-ftb-rm-web-pt/";
+  "https://strangers-things.herokuapp.com/api/2110-ftb-et-web-pt/";
 const API_POSTS = `${BASE_URL}posts`;
 
 const Posts = ({
@@ -13,17 +14,20 @@ const Posts = ({
   setDescription,
   price,
   setPrice,
-  location,
-  setLocation,
+  //   location,
+  //   setLocation,
   willDeliver,
   setWillDeliver,
   userData,
   token,
 }) => {
   const [error, setError] = useState("");
+  const [POST_ID, setPOST_ID] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   //   const { id } = useParams();
 
   console.log(posts);
+  //   console.log(location);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -50,19 +54,53 @@ const Posts = ({
     console.log(info.data.post);
     console.log(info);
     setPosts([...posts, info.data.post]);
+    setPOST_ID(info.data.post._id);
     setTitle("");
     setDescription("");
     setPrice("");
-    setLocation("");
+    // setLocation("");
   };
+
+  const postMatches = (post, text) => {
+    if (
+      post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      post.description.includes(searchTerm) ||
+      post.price.includes(searchTerm) ||
+      post.location.includes(searchTerm)
+    ) {
+      return true;
+    }
+  };
+  const filteredPosts = posts.filter((post) => postMatches(post, searchTerm));
+  const postsToDisplay = searchTerm.length ? filteredPosts : posts;
+
+  console.log(postsToDisplay);
+
   return (
     <>
       <h1 className="main-head">Welcome to Stranger's Things!</h1>
       <h2 id="main-posts">Posts</h2>
-      {userData ? (
-        <div id="create-post">
+      <section id="search-bar">
+        <input
+          value={searchTerm}
+          placeholder="Search Posts"
+          onChange={(e) => {
+            e.preventDefault();
+            setSearchTerm(e.target.value);
+            postMatches;
+          }}
+        ></input>
+      </section>
+      {userData && (
+        <Link className="link" to="/createpost">
           <h3>Create a New Post</h3>
-          <form id="new-post" onSubmit={handleSubmit}>
+        </Link>
+      )}
+      {/* {userData ? (
+        <div id="create-post">
+       
+
+          <form className="new-post" onSubmit={handleSubmit}>
             <input
               className="input-posts"
               type="text"
@@ -92,7 +130,8 @@ const Posts = ({
               onChange={(e) => {
                 setPrice(e.target.value);
               }}
-            ></input>
+            ></input> */}
+      {/* 
             <input
               className="input-posts"
               type="text"
@@ -101,8 +140,9 @@ const Posts = ({
               onChange={(e) => {
                 setLocation(e.target.value);
               }}
-            ></input>
-            <label htmlFor="select-delivery">Delivery?</label>
+            ></input> */}
+
+      {/* <label htmlFor="select-delivery">Delivery?</label>
             <select
               id="select-delivery"
               name="Delivery?"
@@ -118,29 +158,34 @@ const Posts = ({
           </form>
           <p>{error}</p>
         </div>
-      ) : null}
+      ) : null} */}
       <section id="all-posts">
-        {posts.length > 0 &&
-          posts.map((post) => {
-            return (
-              post.active && (
-                <div key={post._id} className="post-results">
-                  <h3 className="post-title">{post.title}</h3>
+        {postsToDisplay.map((post) => {
+          return (
+            post.active && (
+              <div key={post._id} className="post-results">
+                <h3 className="post-title">{post.title}</h3>
 
-                  <p className="post-info">{post.description}</p>
-                  <h4 className="post-info">Price: {post.price}</h4>
-                  <h4 className="post-info">Seller: {post.author.username}</h4>
-                  <h4 className="post-info">Location: {post.location}</h4>
-                  {post.isAuthor ? (
+                <p className="post-info">{post.description}</p>
+                <h4 className="post-info">Price: {post.price}</h4>
+                <h4 className="post-info">Seller: {post.author.username}</h4>
+                <h4 className="post-info">Location: {post.location}</h4>
+                {userData ? (
+                  post.isAuthor ? (
                     <button>DELETE</button>
                   ) : (
-                    <button>SEND MESSAGE</button>
-                  )}
-                  <hr></hr>
-                </div>
-              )
-            );
-          })}
+                    <>
+                      <Link id="send-message" to={`/message/${post._id}`}>
+                        <button>SEND MESSAGE</button>
+                      </Link>
+                    </>
+                  )
+                ) : null}
+                <hr></hr>
+              </div>
+            )
+          );
+        })}
       </section>
     </>
   );
