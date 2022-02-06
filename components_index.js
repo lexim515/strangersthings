a1b6bf4c67bbf30f43bc765c5644583185e0f7bc
Lexi <83599097+lexim515@users.noreply.github.com>
2022-02-06 09:35:06 -0500

    Add files via upload

---
+++ components_index.js
@@ -0,0 +1,9 @@
+export { default as Navbar } from "./Navbar";
+export { default as Home } from "./Home";
+export { default as Posts } from "./Posts";
+export { default as AccountForm } from "./AccountForm";
+export { default as Profile } from "./Profile";
+export { default as Message } from "./Message";
+export { default as CreatePost } from "./CreatePost";
+export { default as EditPost } from "./EditPost";
+export { default as PostDetails } from "./PostDetails";
